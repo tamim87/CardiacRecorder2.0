4cdd00bbc3632e7f229db44b421382612e45a820
mizanurasif <mizanurasif02@gmail.com>
2022-07-28 21:46:25 +0600

    added unit test

--- app/src/test/java/com/example/cardicrecoder/ExampleUnitTest.java
+++ app/src/test/java/com/example/cardicrecoder/ExampleUnitTest.java
@@ -18,4 +18,45 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void testaddrecord(){
+        ArrayList<Recorditem> recorditems = new ArrayList<>();
+        calendar= Calendar.getInstance();
+        String date=new java.text.SimpleDateFormat("dd-MMM-yyyy").format(calendar.getTime());
+        String time=new java.text.SimpleDateFormat("hh:mm a").format(calendar.getTime());
+        Recorditem record=new Recorditem( "afgagawgaw","120","120","80","Normal pressure",date,time,"After Dinner");
+        recorditems.add(record);
+        assertEquals(1,recorditems.size());
+        assertTrue(recorditems.contains(record));
+    }
+
+    @Test
+    public void testdelete()
+    {
+        ArrayList<Recorditem> recorditems = new ArrayList<>();
+        calendar= Calendar.getInstance();
+        String date=new java.text.SimpleDateFormat("dd-MMM-yyyy").format(calendar.getTime());
+        String time=new java.text.SimpleDateFormat("hh:mm a").format(calendar.getTime());
+        Recorditem record=new Recorditem( "afgagawgaw146sgegseh","100","140","80","High pressure",date,time,"After Dinner");
+        recorditems.add(record);
+
+        recorditems.remove(record);
+
+        assertTrue(!recorditems.contains(record));
+
+    }
+    @Test
+    public void testupdate()
+    {
+        ArrayList<Recorditem> recorditems = new ArrayList<>();
+        calendar= Calendar.getInstance();
+        String date=new java.text.SimpleDateFormat("dd-MMM-yyyy").format(calendar.getTime());
+        String time=new java.text.SimpleDateFormat("hh:mm a").format(calendar.getTime());
+        Recorditem record=new Recorditem( "afgagawgaw146sgegseh","100","140","80","High pressure",date,time,"After Dinner");
+        recorditems.add(record);
+        //Recorditem record2=new Recorditem( "afgagawgaw146sgegseh","120","140","80","High pressure",date,time,"After Dinner");
+        recorditems.get(0).setHeart_rate("120");
+        assertTrue(recorditems.contains(record));
+    }
+
 }
